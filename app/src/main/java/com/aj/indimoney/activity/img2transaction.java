a9eeb8f65b797c6327cdc44bf4f838b5a9354bbc
Ajjarvis <archaudhari140@gmail.com>
2021-03-13 19:32:04 +0530

    13 March

---
+++ app/src/main/java/com/aj/indimoney/activity/img2transaction.java
@@ -0,0 +1,14 @@
+package com.aj.indimoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class img2transaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_img2transaction);
+    }
+}
